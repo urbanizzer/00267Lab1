7bcf87b88f4527325d01ff893f68c57896b0be4d
Ali <2016-00267@ac-kuwait.edu.kw>
2018-03-14 10:56:44 +0300

    Initial commit

--- app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
@@ -2,12 +2,44 @@ package com.example.ali.a00267lab1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class ListItemsActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
     }
+
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(ACTIVITY_NAME, "In onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy");
+    }
+
+
 }
--- app/src/main/java/com/example/ali/a00267lab1/StartActivity.java
+++ app/src/main/java/com/example/ali/a00267lab1/StartActivity.java
@@ -2,8 +2,12 @@ package com.example.ali.a00267lab1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +16,32 @@ public class StartActivity extends AppCompatActivity {
 
 
     }
+
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(ACTIVITY_NAME, "In onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy");
+    }
+
+
 }
