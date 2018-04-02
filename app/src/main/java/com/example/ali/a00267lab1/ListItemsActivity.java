3fa99d5ed08aa00a0f5615d7c937695aebe02ec7
Ali <2016-00267@ac-kuwait.edu.kw>
2018-04-03 02:39:51 +0300

    Finished lab 5

--- app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
@@ -1,18 +1,52 @@
 package com.example.ali.a00267lab1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
 
     protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "in onCreate()");
+        Toast.makeText(ListItemsActivity.this, "onCreate test toast", Toast.LENGTH_LONG).show();
+        Switch mySwitch = findViewById(R.id.switch1);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(ListItemsActivity.this, "Switch On", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ListItemsActivity.this, "Switch Off", Toast.LENGTH_SHORT).show();
+                }
+
+                ImageButton fab = findViewById(R.id.imageButton3);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     protected void onStart() {
