2e43baa4a086706c707e7db051cea83fb84cb022
Ali <2016-00267@ac-kuwait.edu.kw>
2018-04-10 10:33:34 +0300

    Finished lab 5

--- app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/ali/a00267lab1/ListItemsActivity.java
@@ -1,12 +1,15 @@
 package com.example.ali.a00267lab1;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -16,7 +19,7 @@ public class ListItemsActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "ListItemsActivity";
     private static final int CAMERA_REQUEST = 1888;
     android.widget.ImageButton ImageButton;
-
+    CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +49,38 @@ public class ListItemsActivity extends AppCompatActivity {
             }
         });
 
+        checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+// 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                                Intent resultIntent = new Intent();
+                                resultIntent.putExtra("Response", "My information to share");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+
+            }
+        });
     }
+
+
+
 
 
     @Override
--- app/src/main/java/com/example/ali/a00267lab1/StartActivity.java
+++ app/src/main/java/com/example/ali/a00267lab1/StartActivity.java
@@ -1,11 +1,13 @@
 package com.example.ali.a00267lab1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -22,12 +24,22 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 5);
             }
         });
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        if (requestCode == 5) {
+            String messagePasssed = data.getStringExtra("Response");
+            if (Activity.RESULT_OK == 1) {
+                Toast.makeText(this, messagePasssed, Toast.LENGTH_LONG);
+            }
+            Log.e(ACTIVITY_NAME, "returned to start");
+        }
     }
 
     protected void onStart() {
